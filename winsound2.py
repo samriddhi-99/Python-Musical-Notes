5f167d19931f9a1f520cf4710f353a7505e2dc30
Samriddhi Srivastava <35305134+samriddhi-99@users.noreply.github.com>
2018-08-22 11:00:32 +0530

    Add files via upload

---
+++ winsound2.py
@@ -0,0 +1,30 @@
+import winsound
+import time
+
+winsound.Beep(659,250 )
+winsound.Beep(622, 250 )
+winsound.Beep(659,250 )
+winsound.Beep(622,250 )
+winsound.Beep(659,250 )
+winsound.Beep(494, 250 )
+winsound.Beep(587,250 )
+winsound.Beep(523,250 )
+
+winsound.Beep(440,500)
+time.sleep(0.25)
+winsound.Beep(262,250)
+winsound.Beep(330,250)
+winsound.Beep(440,250)
+winsound.Beep(494,500)
+time.sleep(0.25)
+winsound.Beep(330,250)
+
+winsound.Beep(415,250)
+winsound.Beep(494,250)
+winsound.Beep(523,500)
+time.sleep(0.25)
+winsound.Beep(330,250)
+
+winsound.Beep(523,250)
+winsound.Beep(494,250)
+winsound.Beep(440,500)
